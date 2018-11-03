482ffa5194e4b4702846d0321092e9b9e1804917
NicerXIII <jessiejerometiquis@gmail.com>
2018-11-02 20:02:01 -0700

    CommitBusnessLayer

---
+++ MVCRepoTest1/MVCRepoTest1.BusinessLayer/EmployeeBusiness.cs
@@ -0,0 +1,18 @@
+﻿using MVCRepoTest1.BusinessLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCRepoTest1.BusinessLayer
+{
+    class EmployeeBusiness : IEmployeeBusiness
+    {
+        public string GetEmployeeName(string EmpID)
+        {
+
+            return "Jessie " + EmpID;
+        }
+    }
+}
