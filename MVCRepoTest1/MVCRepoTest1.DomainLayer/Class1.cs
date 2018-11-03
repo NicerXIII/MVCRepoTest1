07975e94883d60145dd790ddaa0053a3593fe676
NicerXIII <jessiejerometiquis@gmail.com>
2018-11-02 21:59:22 -0700

    test8

--- MVCRepoTest1/MVCRepoTest1.DomainLayer/Class1.cs
+++ MVCRepoTest1/MVCRepoTest1.DomainLayer/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVCDomain
+namespace MVCRepoTest1.DomainLayer
 {
     public class Class1
     {
--- MVCRepoTest1/MVCRepoTest1.DomainLayer/Properties/AssemblyInfo.cs
+++ MVCRepoTest1/MVCRepoTest1.DomainLayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVCDomain")]
+[assembly: AssemblyTitle("MVCRepoTest1.DomainLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MVCDomain")]
+[assembly: AssemblyProduct("MVCRepoTest1.DomainLayer")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("567e934b-97dd-4ba2-b231-dfe6fbc4e59d")]
+[assembly: Guid("d8a52e86-9d2f-400c-a808-228e080839e3")]
 
 // Version information for an assembly consists of the following four values:
 //
